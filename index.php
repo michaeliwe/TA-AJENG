175f3fa115184fb061fc98dccc96a4104c3ee623
Michaeliwe <michael.liwe11@gmail.com>
2019-12-23 16:48:43 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>TA EZPZ</title>
+</head>
+<body>
+<form method='' action='python.php'>
+<input type='submit' value='GO!'>
+</body>
+</html>
---
+++ python.php
@@ -0,0 +1,3 @@
+<?php
+shell_exec('instagram-scraper yusaliano --comments -m 10');
+?>
