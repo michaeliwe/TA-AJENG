d7c3b35178d6a9a42574a601a5fe57272bcefb86
Michaeliwe <michael.liwe11@gmail.com>
2019-12-24 11:12:17 +0800

    test1

--- python.php
+++ python.php
@@ -2,8 +2,8 @@
 $username = $_POST['username'];
 $comments = $_POST['comments'];
 
-shell_exec("instagram-scraper $username --comments -m $comments");
-shell_exec("python excel.py");
+shell_exec('instagram-scraper '.$username.' --comments -m '.$comments);
+shell_exec('python excel.py');
 
 session_start();
 $_SESSION["print"] = "true";
